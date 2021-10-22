504575e527e92e775b823b87adf85d7759bce01b
paruljain1966 <parul.jain@publicissapient.com>
2021-10-22 21:32:53 +0530

    added f2 comment

--- SwiftDemo/SwiftDemo/ViewController2.swift
+++ SwiftDemo/SwiftDemo/ViewController2.swift
@@ -12,6 +12,7 @@ class ViewController2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // f1
+        //f2
 
         // Do any additional setup after loading the view.
     }
